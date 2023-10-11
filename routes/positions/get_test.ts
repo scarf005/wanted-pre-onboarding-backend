9514bc635171f861fdb7df411934502ccb028852
scarf <greenscarf005@gmail.com>
2023-10-12 00:02:14 +0900

    test: 연관 포지션이 모두 같은 회사의 것인지 검사

--- routes/positions/get_test.ts
+++ routes/positions/get_test.ts
@@ -35,8 +35,19 @@ test("GET /positions", { concurrency: true }, async (t) => {
 		assert.equal(res.status, 200)
 
 		const json = await res.json()
-		await t.test("다른 포지션 목록 존재", async () => assert.notEqual(json.otherPositions, []))
-		await t.test("채용 내용이 추가로 담겨 있음", async () => assert.ok(json.description.length > 0))
+
+		t.test("다른 포지션 목록 존재", () => assert.notEqual(json.otherPositions, []))
+		t.test("채용 내용이 추가로 담겨 있음", () => assert.ok(json.description.length > 0))
+
+		await t.test("모두 같은 회사의 포지션", async () => {
+			const assertCompanyIdEquals = (p: { id: number }) =>
+				testApp.positions[":id"].$get({ param: { id: p.id.toString() } })
+					.then((x) => x.json())
+					.then((x) => x.company.id)
+					.then((x) => assert.equal(x, json.company.id))
+
+			await Promise.all(json.otherPositions.map(assertCompanyIdEquals))
+		})
 	})
 
 	await Promise.all([allPositions, findPositions, singlePosition])
