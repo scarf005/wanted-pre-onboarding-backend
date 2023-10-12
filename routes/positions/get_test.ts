40d84910b8ed6bdbd7a4c0e5e654c701d7dc9cc6
scarf <greenscarf005@gmail.com>
2023-10-12 20:53:42 +0900

    test: 검색 실패도 확인

--- routes/positions/get_test.ts
+++ routes/positions/get_test.ts
@@ -12,43 +12,54 @@ test("GET /positions", { concurrency: true }, async (t) => {
 	const app = createApp({ prisma, app: new Hono() })
 	const testApp = testClient(app)
 
-	const allPositions = t.test("전체 포지션 목록", async () => {
+	const allPositions = t.test("채용공고 목록 가져오기", async () => {
 		const res = await testApp.positions.$get({ query: {} })
 		assert.equal(res.status, 200)
 
 		const json = await res.json()
 
-		assert.notEqual(json.length, 0)
+		assert.equal(json.length, 3)
 	})
 
-	const findPositions = t.test("검색 기능", async () => {
+	const searchPositions = t.test("채용공고 검색하기", async () => {
 		const res = await testApp.positions.$get({ query: { search: "네이버" } })
 		assert.equal(res.status, 200)
 
 		const json = await res.json()
 		assert.equal(json.length, 1)
 	})
+	const searchPositionsEmpty = t.test("채용공고 검색 실패시 빈 배열 반환하기", async () => {
+		const res = await testApp.positions.$get({
+			query: { search: "프리온보딩 백엔드 인턴십 선발과제" },
+		})
+		assert.equal(res.status, 200)
 
-	const singlePosition = t.test("단일 포지션 조회", async (t) => {
+		const json = await res.json()
+		assert.equal(json.length, 0)
+	})
+
+	const singlePosition = t.test("채용 상세 페이지 가져오기", async (t) => {
 		const res = await testApp.positions[":id"].$get({ param: { id: "1" } })
 
 		assert.equal(res.status, 200)
 
 		const json = await res.json()
 
-		t.test("다른 포지션 목록 존재", () => assert.notEqual(json.otherPositions, []))
 		t.test("채용 내용이 추가로 담겨 있음", () => assert.ok(json.description.length > 0))
-
-		await t.test("모두 같은 회사의 포지션", async () => {
+		await t.test("다른 채용공고가 추가적으로 포함됨", async (t) => {
 			const assertCompanyIdEquals = (p: { id: number }) =>
 				testApp.positions[":id"].$get({ param: { id: p.id.toString() } })
 					.then((x) => x.json())
 					.then((x) => x.company.id)
 					.then((x) => assert.equal(x, json.company.id))
 
-			await Promise.all(json.otherPositions.map(assertCompanyIdEquals))
+			assert.notEqual(json.otherPositions, [])
+
+			await t.test("모두 같은 회사의 포지션이어야 함", async () => {
+				await Promise.all(json.otherPositions.map(assertCompanyIdEquals))
+			})
 		})
 	})
 
-	await Promise.all([allPositions, findPositions, singlePosition])
+	await Promise.all([allPositions, searchPositions, searchPositionsEmpty, singlePosition])
 })
